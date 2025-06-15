c67f44d4df3f2cc63aedbab04f969dac1e32a47c
wan3761 <2931511040@qq.com>
2025-06-15 21:29:46 +0800

    first version

---
+++ src/main/java/com/wan3761/Main.java
@@ -0,0 +1,12 @@
+package com.wan3761;
+
+/**
+ * @author wan3761
+ * @date ${DATE}
+ * @apiNote
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
